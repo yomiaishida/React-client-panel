a792f3ed2fa12dd219d5f2056405193903d1fc04
Abayomi Aishida <yomicorperate@gmail.com>
2020-07-11 04:16:45 +0100

    added client panel navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AppNavbar from "./components/layout/AppNavbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <AppNavbar />
+        <div className="container">
+          <h1>Hello</h1>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/layout/AppNavbar.js
+++ src/components/layout/AppNavbar.js
@@ -10,7 +10,23 @@ export default class AppNavbar extends Component {
             <Link to="/" className="navbar-brand">
               ClientPanel
             </Link>
-            <button className="navbar-toggler" type="button"></button>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarMain"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarMain">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    Dashboard
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
       </div>
