41830f2af10bc53974cada2e5f2925e39773adf2
Abayomi Aishida <yomicorperate@gmail.com>
2020-07-11 03:57:04 +0100

    initial commit

---
+++ src/components/layout/AppNavbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class AppNavbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
+          <div className="container">
+            <Link to="/" className="navbar-brand">
+              ClientPanel
+            </Link>
+            <button className="navbar-toggler" type="button"></button>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
