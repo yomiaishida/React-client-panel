cb80b08e82a441fafea0f5aca03aee752de571d8
Abayomi Aishida <yomicorperate@gmail.com>
2020-07-17 23:58:43 +0100

    connected Clients component to firebase

--- src/components/clients/clients.js
+++ src/components/clients/clients.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
 
 class Clients extends Component {
   render() {
-    const clients = [
-      {
-        id: "4534543",
-        firstName: "Kevin",
-        lastName: "Johnson",
-        email: "kevin@gmail.com",
-        phone: "555-555-5555",
-        balance: "40",
-      },
-    ];
+    const { clients } = this.props;
 
     if (clients) {
       return (
@@ -62,4 +57,14 @@ class Clients extends Component {
   }
 }
 
-export default Clients;
+Clients.propTypes = {
+  firestore: PropTypes.object.isRequired,
+  clients: PropTypes.array,
+};
+
+export default compose(
+  firestoreConnect([{ collection: "clients" }]),
+  connect((state, props) => ({
+    clients: state.firestore.ordered.clients,
+  }))
+)(Clients);
